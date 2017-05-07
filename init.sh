56b10fd2e805f99c6214726272a6e073dc081f51
gaoxinbo <gaoxinbo1984@gmail.com>
2017-05-07 20:01:17 +0100

    rename venv

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-VIRTUAL_ENV_BASE="venv"
+VIRTUAL_ENV_BASE="venv_dataprovider"
 
 
 if [ ! -d $VIRTUAL_ENV_BASE ]; then 
   virtualenv $VIRTUAL_ENV_BASE 
   . $VIRTUAL_ENV_BASE/bin/activate
-  pip install Flask
 fi
 
-#virtualenv venv
 
+pip install Flask
+pip install gunicorn

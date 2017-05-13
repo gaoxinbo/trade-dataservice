3181e7bfbfc1d16549621137966b3b0a951061b8
gaoxinbo <gaoxinbo1984@gmail.com>
2017-05-13 20:33:33 +0100

    add dummy handler

--- data_provider/data_provider.py
+++ data_provider/data_provider.py
@@ -3,13 +3,16 @@
 
 
 from flask import Flask
-
+from decorator import response_decorator
 
 app = Flask(__name__)
 
+@response_decorator.return_json
+def handler():
+  return "hello"
 
 @app.route("/hello")
 def helo():  
-  return "hello"
+  return handler()
 
 
---
+++ data_provider/decorator/__init__.py
@@ -0,0 +1,2 @@
+
+__all__ = ["response_decorator"]
